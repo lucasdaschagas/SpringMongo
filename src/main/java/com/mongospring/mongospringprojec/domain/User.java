36a285c1174b7ff23a958fc61d9b98d844079b2d
Lucas Souza <lucas.souza@FLEX.LOCAL>
2023-03-13 15:53:09 -0300

    User post reference

--- src/main/java/com/mongospring/mongospringprojec/domain/User.java
+++ src/main/java/com/mongospring/mongospringprojec/domain/User.java
@@ -2,21 +2,21 @@ package com.mongospring.mongospringprojec.domain;
 
 
 import jdk.jfr.Enabled;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.annotation.processing.Generated;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document
 @Setter
+@Getter
 @EqualsAndHashCode(of = "id")
-@AllArgsConstructor
 @NoArgsConstructor
 public class User implements Serializable {
     private static long serialVersionUID = 1L;
@@ -25,16 +25,12 @@ public class User implements Serializable {
     private String id;
     private  String name;
     private  String email;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
 }
